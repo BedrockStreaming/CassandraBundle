fbaa1d8e5d70a547562f8c2f6220d8e7cbfe9877
Stéphane Reuille <sreuille.externe@m6web.fr>
2015-04-30 13:43:06 +0200

    Add Cassandra event

---
+++ src/EventDispatcher/CassandraEvent.php
@@ -0,0 +1,135 @@
+<?php
+namespace M6Web\Bundle\CassandraBundle\EventDispatcher;
+
+use Symfony\Component\EventDispatcher\Event;
+
+/**
+ * Class CassandraEvent
+ */
+class CassandraEvent extends Event
+{
+    /**
+     * Cassandra keyspace where event was dispatched
+     *
+     * @var string
+     */
+    protected $keyspace;
+
+    /**
+     * Cassandra's session command invoked
+     *
+     * @var string
+     */
+    protected $command;
+
+    /**
+     * Command arguments
+     *
+     * @var array
+     */
+    protected $arguments;
+
+    /**
+     * Command execution time
+     *
+     * @var float
+     */
+    protected $executionStart;
+
+    /**
+     * @var float
+     */
+    protected $executionTime;
+
+    /**
+     * @param string $keyspace
+     */
+    public function setKeyspace($keyspace)
+    {
+        $this->keyspace = $keyspace;
+    }
+
+    /**
+     * @return string
+     */
+    public function getKeyspace()
+    {
+        return $this->keyspace;
+    }
+
+    /**
+     * @return array
+     */
+    public function getArguments()
+    {
+        return $this->arguments;
+    }
+
+    /**
+     * @param array $arguments
+     */
+    public function setArguments(array $arguments)
+    {
+        $this->arguments = $arguments;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCommand()
+    {
+        return $this->command;
+    }
+
+    /**
+     * @param string $command
+     */
+    public function setCommand($command)
+    {
+        $this->command = $command;
+    }
+
+    /**
+     * @return float
+     */
+    public function getExecutionStart()
+    {
+        return $this->executionStart;
+    }
+
+    /**
+     * Set execution start of a cassandra request
+     */
+    public function setExecutionStart()
+    {
+        $this->executionStart = microtime(true);
+    }
+
+    /**
+     * Stop the execution of cassandra request
+     * and set the request execution time
+     */
+    public function setExecutionStop()
+    {
+        $this->executionTime = microtime(true) - $this->executionStart;
+    }
+
+    /**
+     * @return float
+     */
+    public function getExecutionTime()
+    {
+        return $this>executionTime;
+    }
+
+    /**
+     * Return execution time in milliseconds
+     *
+     * @return float
+     */
+    public function getTiming()
+    {
+        return $this->getExecutionTime() * 1000;
+    }
+
+}
