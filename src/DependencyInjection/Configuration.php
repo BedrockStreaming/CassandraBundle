199136c353c55ed26a008062b36dfb5424a6641b
Kirill Meretsky <35771790+kyrylich@users.noreply.github.com>
2020-03-16 16:11:26 +0200

    Symfony BC (#44)
    * Add BC to DI Configuration
    
    * Change 'spaceless' to 'apply spaceless' for future versions of Twig
    
    * Manual space removing suggestion

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -17,8 +17,14 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('m6web_cassandra');
+        $treeBuilder = new TreeBuilder('m6web_cassandra');
+
+        if (method_exists($treeBuilder, 'getRootNode')) {
+            $rootNode = $treeBuilder->getRootNode();
+        } else {
+            // BC layer for symfony/config 4.1 and older
+            $rootNode = $treeBuilder->root('m6web_cassandra');
+        }
 
         $rootNode
             ->children()
